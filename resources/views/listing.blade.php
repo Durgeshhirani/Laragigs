b7efafdead6228c7d76700789155f4f4be0a8382
durgesh <durgeshhirani131@gmail.com>
2023-11-21 15:32:26 +0530

    just some images addon and others

--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -1,4 +1,9 @@
 {{-- <h1>{{$id}}</h1> --}}
-<h1>{{$listing['id']}}</h1>
-<h2>{{$listing['title']}}</h2>
-<p>{{$listing['description']}}</p>
+@extends('layout')
+
+@section('content')
+    @include('partials._search')
+    <h1>{{ $listing['id'] }}</h1>
+    <h2>{{ $listing['title'] }}</h2>
+    <p>{{ $listing['description'] }}</p>
+@endsection
